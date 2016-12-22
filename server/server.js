dc9f2ec650c27042b48618df210a60cddbf78966
chen <chen@chendeMacBook-Pro.local>
2016-12-22 11:05:18 +0800

    init

---
+++ server/server.js
@@ -0,0 +1,25 @@
+﻿var express = require('express')
+var rewrite = require('express-urlrewrite')
+var path = require('path')
+var app = express()
+var logger = require('morgan');
+var fs=require("fs");
+
+var onFinished = require('on-finished')
+app.use(logger('dev'));
+app.use(function(req, res, next){
+    // // console.log('服务器重写:', req.url );
+    // // 能夠重写成功
+    // if(req.url.indexOf('.')===-1 && req.url.indexOf('__webpack_hmr')===-1){
+    //     req.url = '/index.html'
+    // }
+    next();
+});
+
+app.use(express.static(path.join(__dirname, '../build')))
+
+
+app.listen(3001, function () {
+  console.log('Server listening on http://localhost:3001, Ctrl+C to stop')
+})
+
